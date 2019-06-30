9c9e977ccfb65656488fd7cd5a820713a8f16b41
Oleg Frey <olfrey@ya.ru>
2019-06-30 03:30:06 +0300

    desktop 95%

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -2,4 +2,28 @@ $(document).ready(function($) {
 
 	$('.input_phone .input__wrap').mask('+7 (000) 000-00-00');
 
+
+	var reviewsList = $('.reviews__list'),
+			reviewsArrows = $('.reviews__arrows'),
+			reviewsCounter = $('.reviews__count-current'),
+			reviewsTotal = $('.reviews__count-total'),
+			reviewsLength = $('.reviews__item').length;
+
+
+	if(reviewsLength < 10) {
+		reviewsTotal.html('0'+reviewsLength);
+	} else{
+		reviewsTotal.html(reviewsLength);
+	}
+
+	reviewsList.slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		adaptiveHeight: true,
+		appendArrows: reviewsArrows,
+	})
+
+	reviewsList.on('afterChange', function(event, slick, direction){
+	  reviewsCounter.html(direction + 1)
+	});
 });
